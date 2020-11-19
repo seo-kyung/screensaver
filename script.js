ad9502760669b88a26a7d4b8a7c990785ca63c06
Seokyung <seokim925@gmail.com>
2020-11-19 16:35:19 +0100

    Basic development
    Basic structure development.
    To be possibly changed: CSS style for making border-line white. Because of the overlapping white lines making the thicker lines, used background-color: white, and make a grid gap 1px.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function setup(){
+function sliderChange(val) {
+    var getnumber = document.getElementById('number').value;
+    // var column_number = Math.round(getnumber * 10)
+    document.getElementById("container").style.gridTemplateColumns = 'repeat(' + getnumber+ ', 1fr)';
 
-    
+    var font_size = 1/getnumber * 5 + 10;
+    var height = 1/getnumber  + 2.5;
+    var paragraph = document.getElementsByClassName("caption_container")
+    var i;
+
+    for (i=0; i< paragraph.length; i++){
+        paragraph[i].style.fontSize = font_size + 'px';
+        paragraph[i].style.height = height +'vw';
+    }
+  
+
 }
+
+
