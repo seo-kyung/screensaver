ff72609c3831776f573e69ccc10c23e2cad409bf
Seokyung <seokim925@gmail.com>
2020-11-20 11:36:48 +0100

    script.js edit
    script.js edit to adjust the height of the caption_container

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function sliderChange(val) {
     document.getElementById("container").style.gridTemplateColumns = 'repeat(' + getnumber+ ', 1fr)';
 
     var font_size = 1/getnumber * 5 + 10;
-    var height = 1/getnumber  + 2.5;
-    var paragraph = document.getElementsByClassName("caption_container")
+    var height = 1/getnumber * 26.4   + 33.6;
+    var paragraph = document.getElementsByClassName("caption_container");
     var i;
 
     for (i=0; i< paragraph.length; i++){
         paragraph[i].style.fontSize = font_size + 'px';
-        paragraph[i].style.height = height +'vw';
+        paragraph[i].style.height = height +'px';
     }
   
 
